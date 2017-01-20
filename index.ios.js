40ab562c11ec8938942141a16b8957e7fb281bf8
Shaan Shetty <srs383@cornell.edu>
2017-01-20 13:44:40 -0500

    first commit

---
+++ index.ios.js
@@ -0,0 +1,15 @@
+//importing libraries
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
+import Home from './src/component/Home';
+
+//create a component
+const Map1 = () =>
+  (
+
+  <Home />
+
+  );
+
+//render
+AppRegistry.registerComponent('Map1', () => Map1);
---
+++ ios/Pods/Target Support Files/Pods-Map1/Pods-Map1-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Map1 : NSObject
+@end
+@implementation PodsDummy_Pods_Map1
+@end
